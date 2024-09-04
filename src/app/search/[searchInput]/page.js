43dcc65b9a7b4062eaddc919abc892bef1f98d60
Chay <chayangkul_c@cmu.ac.th>
2024-09-04 17:08:13 +0700

    atedup2

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -1,36 +1,37 @@
 "use client";
 
-import { MovieRow } from "@/components/MovieRow";
-import { movieDB } from "@/libs/movieDB";
+import MovieRow from "@components/MovieRow";
+import { movieDB } from "@lib/movieDB";
 
 export default function SearchResultPage({ params }) {
-  
-  const searchInput = params.searchInput;
+ 
+  const searchInput = params.searchInput
   const processedSearchInput = searchInput.replaceAll("%20"," ")
 
-  const filteredMovies = movieDB.filter((movie) => 
-  movie.title.tolocaleLowercase().includes(processedSearchInput.tolocaleLowercase())
-);
+  const filteredMovies = movieDB.filter((movie) =>
+    movie.title.toLocaleLowerCase().includes(processedSearchInput.toLocaleLowerCase())
+  );
 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
         Searching &quot; {processedSearchInput} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found {filteredMovies.length} result(s)</p>
-
-  {filteredMovies.map((movie,i) => (
-  
-   <MovieRow
-     key = {movie.id}
-     id = {movie.id}
-     title = {movie.tile}
-     detail = {movie.detail}
-     rating = {movie.detail}
-     number = {i+1}
-   />
-) )
-}
+      <p className="fw-bold fs-4 text-center">
+        Found {filteredMovies.length} result(s)
+      </p>
+      {
+        filteredMovies.map((movie,i) => (
+          <MovieRow
+                key = {movie.id}
+                id = {movie.id}
+                title = {movie.title}
+                detail = {movie.detail}
+                rating = {movie.rating}
+                number = {i + 1}
+          />
+        ))
+        }
     </div>
   );
 }
